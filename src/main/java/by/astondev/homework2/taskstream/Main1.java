0e068986030f4f494c5ffbf3dc30497013ba7d77
PeniazEvgenii <evgenii.peniaz@gmail.com>
2025-06-17 21:57:00 +0300

    TASK2 | change import type java.util.* on full import

--- src/main/java/by/astondev/homework2/taskstream/Main1.java
+++ src/main/java/by/astondev/homework2/taskstream/Main1.java
@@ -3,7 +3,11 @@ package by.astondev.homework2.taskstream;
 import by.astondev.homework2.taskstream.dto.Book;
 import by.astondev.homework2.taskstream.dto.Student;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main1 {
